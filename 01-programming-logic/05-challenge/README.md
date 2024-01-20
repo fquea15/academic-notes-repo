415580d6735e8b7ac462221b7b479ada3a55e471
fquea15 <qjcoders@gmail.com>
2024-01-19 21:07:51 -0500

    Challenge

---
+++ 01-programming-logic/05-challenge/README.md
@@ -0,0 +1,7 @@
+## Achievement and Celebration
+
+The time has come to celebrate a great achievement! In this logic course, all barriers were overcome, and the world of programming was delved into. From setting up the development environment to creating a complete game with various interactions, it has been a journey of mastery.
+
+What's most exciting is that you are now part of the developer community, with access to a vast ecosystem of tools, documentation, and technical support.
+
+Incredible! Here's a quote to celebrate this victory: "Always try to be normal, you'll never know how amazing you can be" (Maya Angelou)
---
+++ 01-programming-logic/05-challenge/app.js
@@ -0,0 +1,38 @@
+//Variables
+let numeroMaximo = 100;
+let numeroSecreto = Math.floor(Math.random() * numeroMaximo) + 1;
+let numeroUsuario = 0;
+let intentos = 1;
+//let palabraVeces = 'vez';
+let maximosIntentos = 6;
+
+console.log(numeroSecreto);
+
+while (numeroUsuario != numeroSecreto) {
+
+    numeroUsuario = parseInt(prompt(`Me indicas un numero entre 1 y ${numeroMaximo} por favor:`));
+    
+    console.log(typeof(numeroUsuario));
+    
+    if (numeroUsuario == numeroSecreto) {
+        //Acertamos, fue verdadera la condicion
+        alert(`Acertaste, el numero es: ${numeroUsuario}. Lo hiciste en ${intentos} ${intentos == 1? 'vez' : 'veces'}`);
+    } else {
+        if (numeroUsuario > numeroSecreto) {
+            alert('el numero secreto es menor');
+        } else {
+            alert('El numero secreto es mayor');
+        }
+        //incremenatmos el contador
+        //intentos += 1;
+        intentos++;
+        //palabraVeces = 'veces';
+        
+        if (intentos > maximosIntentos) {
+            alert(`Llegaste al numero maximo de ${maximosIntentos} intentos `);
+            break;
+        }
+        //La condicion no se complio..
+        //alert('Lo siento, no acertaste el numero');
+    }
+}
